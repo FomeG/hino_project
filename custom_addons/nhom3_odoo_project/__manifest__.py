ea49e9257d23db458c0a181e39f76c5aea899387
Lê Chính Đại <140992360+RelliZisme98@users.noreply.github.com>
2025-02-05 12:16:25 +0700

    ADD Tab Promotions
    add tab promotion

--- custom_addons/nhom3_odoo_project/__manifest__.py
+++ custom_addons/nhom3_odoo_project/__manifest__.py
@@ -10,6 +10,7 @@
         # 'views/repair_order_vehicle_views.xml',
         'views/tab_repair_history_views.xml',
         'views/tab_service_info_worklist_service_views.xml',
+        'views/tab_promotions_program_views.xml',
         'views/view.xml',
         'views/menus.xml',
     ],
