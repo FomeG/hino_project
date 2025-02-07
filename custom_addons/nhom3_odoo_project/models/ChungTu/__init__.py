113ff24689a4e9585da2b91fc9fbd8c2ba3b120e
Nguyen Hieu <99974579+nguyenhieuxyz@users.noreply.github.com>
2025-02-07 16:35:25 +0700

    [IMP] Improve __manifest__.py, tab_customer /// List view (#18)
    * [ADD/IMP] Add contact smart button logic
    
    * [ADD/IMP] Add contact smart button logic
    
    * [ADD/IMP] Add contact smart buttons, Customer vehicle tab, Impove ct_thongtinxe
    
    * [IMP] Improve ct_thongtinxe.py and tab_customer_vehicle_info.py

--- custom_addons/nhom3_odoo_project/models/ChungTu/__init__.py
+++ custom_addons/nhom3_odoo_project/models/ChungTu/__init__.py
@@ -1,5 +1,8 @@
+# Model for Repair Order Headers
 from . import ct_chuxe
 from . import ct_header
 from . import ct_thongtinchung
 from . import ct_thongtinxe
 from . import list
+
+# Model for Repair Quotation Headers
--- custom_addons/nhom3_odoo_project/models/Tabs/__init__.py
+++ custom_addons/nhom3_odoo_project/models/Tabs/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+# Model for Repair Order Tabs
 from . import tab_repair_history
 from . import tab_service_sparepart_list
 from . import tab_service_info_document
@@ -8,3 +9,6 @@ from . import tab_promotions_program
 from . import tab_promotions_applies
 from . import tab_sale_order
 from . import tab_customer_vehicle_info
+
+# Model for Repair Quotation Tabs
+
