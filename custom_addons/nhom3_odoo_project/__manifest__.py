14a6de744bccd7f13a2ee9aa2689a22cf6a87f1e
Trọng Nghĩa <163431212+FomeG@users.noreply.github.com>
2025-02-10 17:07:43 +0700

    [FIX/IMP] Feature/bang gia (#26)
    * [IMP] Improve ct_thongtinxe.py and tab_customer_vehicle_info.py
    
     [IMP] Improve ct_thongtinxe.py and tab_customer_vehicle_info.py
    
    Cải thiện lại chứng từ thông tin xe
    Cải thiện lại tab thông tin xe của khách hàng
    
    * [ADD] [BANGGIA-2.4] Add Price List Menu, Tabs and their corresponding fields
    
    * [IMP] [BANGGIA_2.4] Fix Price List Extend Logic (Remove discount constrain)
    
    * [IMP] Group banggia into nhom3_odoo_project (common module folder)
    
    * [ADD] Customer Signature Tab
    
    * [FIX] Temporarily remove customer signature functions
    
    ---------
    
    Co-authored-by: Nguyen Hieu <99974579+nguyenhieuxyz@users.noreply.github.com>

--- custom_addons/nhom3_odoo_project/__manifest__.py
+++ custom_addons/nhom3_odoo_project/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    'name': 'Lệnh sửa chữa',
+    'name': 'Repair Order',
     'version': '17.0.1.0',
     'author': 'Le Chinh Dai',
     'license': 'LGPL-3',
@@ -20,11 +20,21 @@
         'views/repair_order_list_view.xml',
         'views/smart_buttons_view.xml',
         'views/tab_customer_vehicle_info_view.xml',
+        
+        
+        #region BẢNG GIÁ
+        'views/product_pricelist_views.xml',
+        'views/product_pricelist_item_views.xml', 
+        #endregion
+        
+        
         'views/tad_order_detail.xml',
         'views/tab_customer_signature.xml',
         'views/menus.xml',
     ],
+    
     'installable': True,
     'application': True,
-    'sequence': 1
+    'auto_install': False,
+    'sequence': 1,  # Đưa Module lên đầu
 }
